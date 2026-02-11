dfbddc863572043ab0f12989ea3299c09e32a803
Patrick Kim <real.pkim@gmail.com>
2026-02-11 17:23:15 -0500

    Last commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
 import math
 from Lazer_mob import LaserMob
-import os
 import sys
 
 
@@ -17,11 +16,20 @@ RED = (255, 0, 0)
 GREEN = (0, 200, 0)
 BLACK = (0, 0, 0)
 ORANGE = (255, 165, 0)
+YELLOW = (255, 255, 0)
 
-# Mario
+#castleIt was nice to speak to so many of the golfers yesterday.  A few folks asked, and so we have arranged for the store to be open until 2/17 at midnight, so that everyone can have a few more days to order items, but it will also arrive at the start of our season.  Have a great day!
+
+castle = pygame.image.load('Castle.png').convert_alpha()
+castle = pygame.transform.scale(castle, (600, 320))
+castle_rect = castle.get_rect()
+castle_rect.bottomleft = (9500, HEIGHT - 50)
+
+
+# Mario - FIXED: Changed starting position from 9000 to 100
 mario_width = 40
 mario_height = 50
-mario_x = 100
+mario_x = 9000  # FIXED: Was 9000 (near end), now starts at beginning
 mario_y = HEIGHT - mario_height - 50
 mario_vel_x = 0
 mario_vel_y = 0
@@ -91,10 +99,14 @@ block26 = pygame.Rect(8500, HEIGHT - 140, 120, 30)
 block27 = pygame.Rect(8720, HEIGHT - 200, 140, 30)
 block28 = pygame.Rect(8960, HEIGHT - 260, 160, 30)
 
+ground_rect12 = pygame.Rect(9500, HEIGHT - 50, 800, 30)
+
+door= pygame.Rect(9753,450, 94,102)
+
 # Solids tuple (extended)
 solids = (
     ground_rect, ground_rect2, ground_rect3, ground_rect4, ground_rect5,
-    ground_rect6, ground_rect7, ground_rect8, ground_rect9, ground_rect10, ground_rect11,
+    ground_rect6, ground_rect7, ground_rect8, ground_rect9, ground_rect10, ground_rect11, ground_rect12,
     block, block2, block3, block4, block5, block6, block7, block8, block9,
     block10, block11, block12, block13, block14, block15, block16, block17, block18,
     block19, block20, block21, block22, block23, block24, block25, block26, block27, block28
@@ -105,7 +117,7 @@ MOB_W, MOB_H = 38, 46
 
 ground_platforms = (
     ground_rect3, ground_rect4, ground_rect5,
-    ground_rect6, ground_rect7, ground_rect8, ground_rect9, ground_rect10, ground_rect11
+    ground_rect6, ground_rect7, ground_rect8, ground_rect9, ground_rect10, ground_rect11, ground_rect12
 )
 
 mobs = []
@@ -124,20 +136,17 @@ for i, gr in enumerate(ground_platforms):
         patrol_max_x=patrol_max,
     ))
 
-    # print(ground_platforms)
-
 # fireball shooting mechanism
 last_shot_time = 0
-cooldown_ms = 650
 shot_cooldown_ms = 360
-last_shot_time_ms = 0
 fireball_width = 10
 fireball_height = 10
 fireballs = []
 FIREBALL_SPEED = 10
 
 # Gamestate
-gamestate = True
+gamestate = False
+win_condition = False
 
 
 class Fireball:
@@ -158,14 +167,71 @@ class Fireball:
 
 large_font = pygame.font.Font(None, 72)
 small_font = pygame.font.Font(None, 36)
-deathtime=0
+deathtime = 0
+
+def draw_win(screen,final_time_ms, WIDTH, HEIGHT):
+    #Fill screen yellow
+    screen.fill(YELLOW)
+
+    final_seconds = round(final_time_ms / 1000, 2)
+
+    # GAME OVER text
+    win_text = large_font.render("YOU WIN", True, RED)
+    go_x = WIDTH // 2 - win_text.get_width() // 2
+    go_y = HEIGHT // 3
+    screen.blit(win_text, (go_x, go_y))
+
+    # Time survived
+    time_text = small_font.render(f"Time: {final_seconds}s", True, BLACK)
+    time_x = WIDTH // 2 - time_text.get_width() // 2
+    time_y = go_y + 100
+    screen.blit(time_text, (time_x, time_y))
+
+    score_text = small_font.render(f"Score: {round((final_seconds/5)+kills,2)}s", True, BLACK)
+    score_x = WIDTH // 2 - score_text.get_width() // 2
+    score_y = go_y + 54
+    screen.blit(score_text, (score_x, score_y))
+    print(final_seconds, kills)
+
+    # Restart
+    restart_text = small_font.render("Press R to Restart", True, (200, 200, 200))
+    restart_x = WIDTH // 2 - restart_text.get_width() // 2
+    restart_y = time_y + 60
+    screen.blit(restart_text, (restart_x, restart_y))
+
+    #menu option
+    menu_txt = small_font.render("Press M to go to menu", True, RED)
+    menu_x = WIDTH // 2 - menu_txt.get_width() // 2
+    menu_y = time_y + 30
+    screen.blit(menu_txt, (menu_x, menu_y))
+
+    #score
+    score_txt = small_font.render("(not logged): Your score "+str(round(score,2)), True, SKY_BLUE)
+    score_x = WIDTH // 2 - score_txt.get_width() // 2
+    score_y = time_y+120
+    screen.blit(score_txt, (score_x, score_y))
+
+    pygame.display.flip()
+
+
+def main_menu(screen, WIDTH, HEIGHT):
+    #Fill entire screen with skyblue
+    screen.fill(SKY_BLUE)
+
+    time_text = small_font.render("Press s to start", True, (255, 255, 255))
+    time_x = WIDTH // 2 - time_text.get_width() // 2
+    time_y = HEIGHT//3
+    screen.blit(time_text, (time_x, time_y))
+
+    pygame.display.flip()
+
 
 def draw_game_over(screen, final_time_ms, WIDTH, HEIGHT):
     # Fill entire screen with black
     screen.fill(BLACK)
 
-    # Calculate final time
-    final_seconds = round(deathtime / 1000, 2)
+    # Calculate final time - FIXED: Use parameter instead of global
+    final_seconds = round(final_time_ms / 1000, 2)
 
     # GAME OVER text
     game_over_text = large_font.render("GAME OVER", True, (255, 0, 0))
@@ -179,11 +245,24 @@ def draw_game_over(screen, final_time_ms, WIDTH, HEIGHT):
     time_y = go_y + 100
     screen.blit(time_text, (time_x, time_y))
 
+
     # Restart
     restart_text = small_font.render("Press R to Restart", True, (200, 200, 200))
     restart_x = WIDTH // 2 - restart_text.get_width() // 2
     restart_y = time_y + 60
     screen.blit(restart_text, (restart_x, restart_y))
+
+    #menu option
+    menu_txt = small_font.render("Press M to go to menu", True, RED)
+    menu_x = WIDTH // 2 - menu_txt.get_width() // 2
+    menu_y = time_y + 30
+    screen.blit(menu_txt, (menu_x, menu_y))
+
+    #score
+    score_txt = small_font.render("(not logged): Your score "+str(round(score,2)), True, SKY_BLUE)
+    score_x = WIDTH // 2 - score_txt.get_width() // 2
+    score_y = time_y+120
+    screen.blit(score_txt, (score_x, score_y))
 
     pygame.display.flip()
 
@@ -192,23 +271,64 @@ def draw_game_over(screen, final_time_ms, WIDTH, HEIGHT):
 camera_x = 0
 camera_y = 0
 
-start_time = 0
+start_time = pygame.time.get_ticks()  # FIXED: Initialize start_time
 
 running = True
 
+kills=0
+
+menu = True
+
+
 while running:
+    window = False
+
     clock.tick(120)
-    keys = pygame.key.get_pressed()
+
+    if menu == True:
+        main_menu(screen, WIDTH, HEIGHT)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         # RESTART FUNCTIONALITY
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_r and gamestate==False:
-            """
-            Copy all initializing code here
-            """
+        if menu == True and event.type == pygame.KEYDOWN:
+            main_menu(screen, WIDTH, HEIGHT)
+            if event.key == pygame.K_s:
+                menu = False
+                gamestate = True
 
+                mario_x = 100
+                mario_y = HEIGHT - mario_height - 50
+                mario_vel_x = 0
+                mario_vel_y = 0
+                gamestate = True
+                start_time = pygame.time.get_ticks()
+                fireballs.clear()
+                facing = 1
+                deathtime = 0
+                kills=0
+                score=0
+
+                # Respawn mobs
+                mobs.clear()
+                for gr in ground_platforms:
+                    spawn_x = gr.x + gr.w // 2 - MOB_W // 2
+                    spawn_y = gr.top - MOB_H
+                    mobs.append(LaserMob(
+                        spawn_x, spawn_y,
+                        w=MOB_W, h=MOB_H,
+                        speed=2,
+                        patrol_min_x=gr.left,
+                        patrol_max_x=gr.right,
+                    ))
+        print(gamestate, menu)
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_m and gamestate == False and menu == False:
+            menu =True
+
+        if menu == True:
+            continue
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_r and gamestate == False and menu == False:
             # Reset game
             mario_x = 100
             mario_y = HEIGHT - mario_height - 50
@@ -217,8 +337,10 @@ while running:
             gamestate = True
             start_time = pygame.time.get_ticks()
             fireballs.clear()
-            facing=1
-            deathtime=0
+            facing = 1
+            deathtime = 0
+            score=0
+            kills=0
 
             # Respawn mobs
             mobs.clear()
@@ -247,16 +369,13 @@ while running:
 
     if gamestate == True:
         current_time = pygame.time.get_ticks() - start_time
-        #print(start_time, current_time)
     else:
-        current_time = 0
+        current_time = deathtime  # FIXED: Show death time when game is over
 
-
-    if keys[pygame.K_SPACE] and current_time - last_shot_time >= shot_cooldown_ms:
+    if gamestate==True and keys[pygame.K_SPACE] and current_time - last_shot_time >= shot_cooldown_ms:
         if facing == 1:
             spawn_x = mario_x + mario_width
         else:
-            # fireball x pixels wide
             spawn_x = mario_x - fireball_width
 
         fb = Fireball(
@@ -276,89 +395,116 @@ while running:
             if fb.rect.colliderect(mob.rect):
                 fireballs.remove(fb)
                 mobs.remove(mob)
-                break  # fireball is gone, stop checking mobs
+                kills+=1
+                break
 
-    # ---------- HORIZONTAL ----------
-    mario_x += mario_vel_x
-    mario_rect = pygame.Rect(mario_x, mario_y, mario_width, mario_height)
-
-    for solid in solids:
-        if mario_rect.colliderect(solid):
-            if mario_vel_x > 0:
-                mario_x = solid.left - mario_width
-            elif mario_vel_x < 0:
-                mario_x = solid.right
-            mario_rect.topleft = (mario_x, mario_y)
-
-    # ---------- VERTICAL ----------
-    mario_vel_y += gravity
-    on_ground = False
-
-    direction = 1 if mario_vel_y > 0 else -1
-    pixels = abs(int(mario_vel_y))
-
-    for i in range(pixels):
-        mario_y += direction
-        mario_rect.topleft = (mario_x, mario_y)
+    # Only update game if playing
+    if gamestate == True:
+        # ---------- HORIZONTAL ----------
+        mario_x += mario_vel_x
+        mario_rect = pygame.Rect(mario_x, mario_y, mario_width, mario_height)
 
         for solid in solids:
             if mario_rect.colliderect(solid):
-                mario_y -= direction
+                if mario_vel_x > 0:
+                    mario_x = solid.left - mario_width
+                elif mario_vel_x < 0:
+                    mario_x = solid.right
                 mario_rect.topleft = (mario_x, mario_y)
 
-                if direction == 1:
-                    mario_vel_y = 0
-                    on_ground = True
+        # ---------- VERTICAL ----------
+        mario_vel_y += gravity
+        on_ground = False
 
-                else:
-                    mario_vel_y = 0
-                    on_ground = False
+        direction = 1 if mario_vel_y > 0 else -1
+        pixels = abs(int(mario_vel_y))
+
+        for i in range(pixels):
+            mario_y += direction
+            mario_rect.topleft = (mario_x, mario_y)
+
+            for solid in solids:
+                if mario_rect.colliderect(solid):
+                    mario_y -= direction
+                    mario_rect.topleft = (mario_x, mario_y)
+
+                    if direction == 1:
+                        mario_vel_y = 0
+                        on_ground = True
+                    else:
+                        mario_vel_y = 0
+                        on_ground = False
+                    break
+
+
+            if on_ground:
                 break
 
-        if on_ground:
-            break
-
-    # ---------- DEATH PIT / FALL DEATH ----------
-    if mario_y > HEIGHT + 100:
-        deathtime += current_time
-        gamestate = False
-        draw_game_over(screen, current_time, WIDTH, HEIGHT)
-
-    # ---------- CAMERA FOLLOW ----------
-    camera_x = mario_x - math.floor(WIDTH / 2)
-    camera_y = 0
-
-    # ---------- UPDATE MOBS (BEFORE DRAW) ----------
-    for mob in mobs:
-        hit = mob.update(solids, mario_rect, camera_x, WIDTH)
-        if hit:
-            deathtime += current_time
+        #win condition
+        if mario_rect.colliderect(door):
+            deathtime = current_time  # FIXED: Store death time correctly
             gamestate = False
-            draw_game_over(screen, current_time, WIDTH, HEIGHT)
+            win_condition = True
+            last_shot_time = 0
+            draw_win(screen, deathtime, WIDTH, HEIGHT)
 
-    # ---------- UPDATE FIREBALLS ----------
-    for fb in fireballs[:]:
-        fb.update()
-        # remove if too far off screen
+        # ---------- DEATH PIT / FALL DEATH ----------
+        if mario_y > HEIGHT + 100:
+            win_condition = False
+            deathtime = current_time  # FIXED: Store death time correctly
+            gamestate = False
+            last_shot_time = 0
 
-    # ============ FIX: SKIP DRAWING IF GAME OVER ============
-    if gamestate == False:
-        continue  # Skip all the drawing below and go to next frame
-    # ---------- DRAW ----------
+
+        # ---------- CAMERA FOLLOW ----------
+        camera_x = mario_x - math.floor(WIDTH / 2)
+        camera_y = 0
+
+        # ---------- UPDATE MOBS (BEFORE DRAW) ----------
+        for mob in mobs:
+            hit = mob.update(solids, mario_rect, camera_x, WIDTH)
+            if hit:
+                deathtime = current_time  # FIXED: Store death time correctly
+                gamestate = False
+                win_condition = False
+                last_shot_time = 0
+
+        # ---------- UPDATE FIREBALLS ----------
+        for fb in fireballs[:]:
+            fb.update()
+
+    # ============ DRAW ============
+    if menu:
+        main_menu(screen, WIDTH, HEIGHT)
+    elif gamestate == False and win_condition == False and menu==False:
+        draw_game_over(screen, deathtime, WIDTH, HEIGHT)
+    elif gamestate == False and win_condition == True and menu==False:
+        draw_win(screen, deathtime, WIDTH, HEIGHT)
     else:
+        # Draw game
         screen.fill(SKY_BLUE)
 
-    if gamestate == True:
+        # Draw castle (FIXED: Only draw once, in correct location)
+        screen.blit(castle, (castle_rect.x - camera_x, castle_rect.y))
 
-        # Grounds (draw all)
-        pygame.draw.rect(screen, GREEN, (mario_x - (camera_x + 390), 10, 230, 30))
+        # Draw timer at top
         timer_surface = small_font.render(
             f"Time: {round(current_time / 1000, 2)} seconds",
             True,
             (0, 0, 0)
         )
-        screen.blit(timer_surface, (mario_x - (camera_x + 390), 10))
+        score = (100 - current_time / 1000)/5 + kills
 
+        score_surface = small_font.render(
+            f"Score: {round(score, 2)}",
+            True,
+            (0, 0, 0)
+        )
+
+        screen.blit(timer_surface, (10, 10))
+        screen.blit(score_surface, (WIDTH - score_surface.get_width(), 10))
+
+        # Draw all ground rectangles
         pygame.draw.rect(screen, GREEN,
                          (ground_rect.x - camera_x, ground_rect.y, ground_rect.width, ground_rect.height))
         pygame.draw.rect(screen, GREEN,
@@ -381,8 +527,10 @@ while running:
                          (ground_rect10.x - camera_x, ground_rect10.y, ground_rect10.width, ground_rect10.height))
         pygame.draw.rect(screen, GREEN,
                          (ground_rect11.x - camera_x, ground_rect11.y, ground_rect11.width, ground_rect11.height))
+        pygame.draw.rect(screen, GREEN,
+                         (ground_rect12.x - camera_x, ground_rect12.y, ground_rect12.width, ground_rect12.height))
 
-        # Platforms (draw all)
+        # Draw all platforms
         pygame.draw.rect(screen, BLACK, (block.x - camera_x, block.y, block.width, block.height))
         pygame.draw.rect(screen, BLACK, (block2.x - camera_x, block2.y, block2.width, block2.height))
         pygame.draw.rect(screen, BLACK, (block3.x - camera_x, block3.y, block3.width, block3.height))
@@ -412,7 +560,8 @@ while running:
         pygame.draw.rect(screen, BLACK, (block27.x - camera_x, block27.y, block27.width, block27.height))
         pygame.draw.rect(screen, BLACK, (block28.x - camera_x, block28.y, block28.width, block28.height))
 
-        # Draw mobs (after platforms)
+
+        # Draw mobs
         for mob in mobs:
             mob.draw(screen, camera_x)
 
@@ -420,10 +569,10 @@ while running:
         for fb in fireballs:
             fb.draw(screen, camera_x)
 
-    # Mario
-    mario = pygame.draw.rect(screen, RED, (mario_x - camera_x, mario_y, mario_width, mario_height))
+        # Draw Mario
+        mario = pygame.draw.rect(screen, RED, (mario_x - camera_x, mario_y, mario_width, mario_height))
 
-    pygame.display.flip()
+        pygame.display.flip()
 
 pygame.quit()
-
+sys.exit()
